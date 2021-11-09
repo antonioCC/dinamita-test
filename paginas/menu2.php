2f3be38a9fba5d51ec9c9b79a580e168c655a038
Gener-Poot <gener.pootcan@itsva.edu.mx>
2021-11-08 22:26:05 -0600

    Subiendo archivos php en la carpeta paginas

---
+++ paginas/menu2.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<!--link href='http://fonts.googleapis.com/css?family=Arvo:400,700|Pacifico|Playball|Pinyon+Script' rel='stylesheet' type='text/css'>-->
+
+<link rel="stylesheet" href="../estilos/index.css">
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js">
+  </script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js">
+  </script>
+
+</head>
+
+<body>
+<div id="contenedor">
+<header>
+	<figure>
+  		<img id="panoramica" src="../imagenes/banner.png" alt="Fachada normal JDRH" />
+      <img id="logonormal" src="../imagenes/dt.jpeg" alt="Logo normal JDRH" />
+	</figure>
+	<h1>Taqueria "Don Toño"</h1>
+  
+</header>
+
+
+
+<nav class="nav">
+  <div class="container-fluid">
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right" id=menu>
+        <li> <a href="../index.php">logout</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+
+
+
+<div id="sect">
+<section>
+<h2>menu</h2>
+
+
+
+<p><img src="../imagenes/TORTAS.jpg" alt="Logo n"/><h3>tortas</h3> (descripcion) asasajlshdljslvj slkjsdjcsjcl sjdclskdjlksd 
+jlsjdcl ksdjclskjdclksdjckl sd jlcsjd lcjsdkl cjdhkdjahsdkjas hkdjahskdhaksjd asdakshdk ajsdhkajsdhkj <span>agregar</span></p>
+<br>
+<p><img src="../imagenes/tacos.jpg" alt="Logo n"/><h3>tacos</h3> (descripcion) asasa jlshdljslvjsl kjsdjcsjcl sjdclskd jlk sdjl sjdc lksdjclskjdclksdj cklsdjlcs jdlcjsdklcjdhkdjahsdkjashkdjahskdhaksjd asdakshdkajsdhkajsdhkj <span>agregar</span></p>
+<br>
+<p><img src="../imagenes/bb.jpg" alt="Logo n"/><h3>bebidas</h3> (descripcion) asasa jlshdljslvjsl kjsdjcsjcl sjdclskd jlk sdjl sjdc lksdjclskjdclksdj cklsdjlcs jdlcjsdklcjdhkdjahsdkjashkdjahskdhaksjd asdakshdkajsdhkajsdhkj <span>agregar</span></p>
+
+
+</section>
+</div>
+
+<footer>
+Copyright antonio,diana,felipe
+</footer>
+</div>
+</body>
+</html>
---
+++ paginas/principal.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<!--link href='http://fonts.googleapis.com/css?family=Arvo:400,700|Pacifico|Playball|Pinyon+Script' rel='stylesheet' type='text/css'>-->
+
+<link rel="stylesheet" href="../estilos/index.css">
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js">
+  </script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js">
+  </script>
+
+</head>
+
+<body>
+<div id="contenedor">
+<header>
+	<figure>
+  		<img id="panoramica" src="../imagenes/banner.png" alt="Fachada normal JDRH" />
+      <img id="logonormal" src="../imagenes/dt.jpeg" alt="Logo normal JDRH" />
+	</figure>
+	<h1>Taqueria "Don Toño"</h1>
+  
+</header>
+
+
+
+<nav class="nav">
+  <div class="container-fluid">
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav" id=menu>
+        <li> <a href="roles.php">roles</a></li>
+        <li> <a href="usuarios.php">usuarios</a></li>
+        <li> <a href="perfiles.php">perfiles</a></li>
+        <li> <a href="informacion.html">producto</a></li>
+        <li> <a href="ventas.php">ventas</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right" id=menu>
+        <li> <a href="../index.php">logout</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+
+
+
+<div id="sect">
+<section>
+<h2 style="text-align: center;">bienvenido poderoso admin</h2>
+
+
+
+
+
+
+</section>
+</div>
+
+<footer>
+Copyright antonio,diana,felipe
+</footer>
+</div>
+</body>
+</html>
